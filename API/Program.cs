78893f002b4c93168865f2162749154a7bdcffdc
Yodiculta <dbautova@yandex.ru>
2023-10-14 18:11:43 +0300

    Cors Policy and semantic UI

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnction"));
 });
 
+builder.Services.AddCors(opt =>{
+    opt.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,8 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
+// app.UseHttpsRedirection();
+app.UseCors("CorsPolicy");
 app.UseAuthorization();
 
 app.MapControllers();
